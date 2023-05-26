7cb3e197e477d94ad980f855b25bc9cdb8549786
vic0rv <victor30072002@hotmail.com>
2023-05-26 14:45:00 +0200

    Aparece listado de peliculas pero con detalles

--- app/src/main/java/com/example/aplicacion_peliculas_okode/APIConnection.java
+++ app/src/main/java/com/example/aplicacion_peliculas_okode/APIConnection.java
@@ -1,39 +1,40 @@
 package com.example.aplicacion_peliculas_okode;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class APIConnection {
-    public static void main(String[] args){
-        try{
-            URL url = new URL("https://api.themoviedb.org/3/movie/11?api_key=0b5a2d0df283cf542cc09cc420f24fa4");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Lee la respuesta de la API
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                StringBuilder response = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-                reader.close();
 
-                // Procesa la respuesta de la API
-                System.out.println(response.toString());
-            } else {
-                // Maneja el error de la respuesta
-                System.out.println("Error: " + responseCode);
+
+    public void getPeliculasPopulares(CallbackPeliculas callback) {
+        OkHttpClient client = new OkHttpClient();
+
+
+        Request request = new Request.Builder().url("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1")
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjVhMmQwZGYyODNjZjU0MmNjMDljYzQyMGYyNGZhNCIsInN1YiI6IjY0NmY2NzNjMTdjNDQzMDExOWIwZTg2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TpxLlj869gKpa4THo5b_33_1_Boc8ziDMwXrvvKLqaY")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Manejo de errores en caso de fallo de la solicitud
+                callback.onErrorPeliculas(e);
             }
 
-            // Cierra la conexión
-            connection.disconnect();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String responseBody = response.body().string();
+                callback.onResponsePeliculas(responseBody);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/aplicacion_peliculas_okode/MainActivity.java
+++ app/src/main/java/com/example/aplicacion_peliculas_okode/MainActivity.java
@@ -3,13 +3,42 @@ package com.example.aplicacion_peliculas_okode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
 
+
+public class MainActivity extends AppCompatActivity implements CallbackPeliculas {
+    APIConnection api = new APIConnection();
+    TextView prueba;
+    String respuesta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prueba = findViewById(R.id.tv1);
+        api.getPeliculasPopulares(this);
+
+
+    }
+
+    @Override
+    public void onResponsePeliculas(String responseBody) {
+        respuesta = responseBody;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                prueba.setText(respuesta);
+            }
+        });
+    }
+
+    @Override
+    public void onErrorPeliculas(IOException e) {
+        respuesta = e.toString();
+        //Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
 
     }
 }
